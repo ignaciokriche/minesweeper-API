596674e87cdab4dbce58e8bbe8171c8968db4b3d
ignaciokriche <ignaciokriche@gmail.com>
2020-10-08 12:21:29 -0300

    unit testing then Board class.

--- src/test/java/ar/com/kriche/minesweeper/domain/BoardTest.java
+++ src/test/java/ar/com/kriche/minesweeper/domain/BoardTest.java
@@ -33,7 +33,7 @@ public class BoardTest {
         int columnLength = 10;
         int mines = 7;
 
-        List mineLocationsList = Arrays.asList(
+        List mineLocations = Arrays.asList(
                 false, false, false, false, true, false, false, false, false, true,
                 false, false, false, false, false, false, false, false, false, false,
                 false, false, false, false, true, false, false, false, false, false,
@@ -60,18 +60,63 @@ public class BoardTest {
                 0, 1, 1, 1, 0, 0, 0, 0, 0, 0,
         };
 
-        Mockito.when(randomService.shuffledBooleans(mines, rowLength * columnLength)).thenReturn(mineLocationsList);
+        Mockito.when(randomService.shuffledBooleans(mines, rowLength * columnLength)).thenReturn(mineLocations);
 
         // exercise:
         Board actualBoard = new Board(randomService);
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("actual board:\n" + actualBoard);
+        }
 
         // verify:
-
         for (int r = 0; r < rowLength; r++) {
             for (int c = 0; c < columnLength; c++) {
                 Cell actualCell = actualBoard.cellAt(r, c);
                 assertEquals("at row: " + r + ", col: " + c,
                         expectedMineCounts[r * columnLength + c], actualCell.getAdjacentMines());
+            }
+        }
+        Mockito.verify(randomService).shuffledBooleans(mines, rowLength * columnLength);
+
+    }
+
+    @Test
+    public void givenMineLocationsWhenCreatingBoardThenMinesMustInTheRightCells() {
+
+        // setup:
+
+        int rowLength = 10;
+        int columnLength = 10;
+        int mines = 7;
+
+        List mineLocations = Arrays.asList(
+                true, false, false, false, false, false, false, false, false, false,
+                true, false, false, false, false, false, false, false, true, false,
+                false, false, false, false, false, false, false, false, false, false,
+                false, false, false, false, true, false, false, false, false, false,
+                false, false, false, false, false, false, false, false, false, false,
+                false, false, false, true, false, false, false, false, false, false,
+                false, false, false, false, false, false, false, false, false, false,
+                false, false, false, false, false, false, false, true, false, false,
+                false, false, false, false, true, false, false, false, false, false,
+                false, false, false, false, false, false, false, false, false, false
+        );
+
+        Mockito.when(randomService.shuffledBooleans(mines, rowLength * columnLength)).thenReturn(mineLocations);
+
+        // exercise:
+        Board actualBoard = new Board(randomService);
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("actual board:\n" + actualBoard);
+        }
+
+
+        // verify:
+        for (int r = 0; r < rowLength; r++) {
+            for (int c = 0; c < columnLength; c++) {
+                Cell actualCell = actualBoard.cellAt(r, c);
+                assertEquals("at row: " + r + ", col: " + c,
+                        mineLocations.get(r * columnLength + c), actualCell.isMined());
             }
         }
         Mockito.verify(randomService).shuffledBooleans(mines, rowLength * columnLength);
